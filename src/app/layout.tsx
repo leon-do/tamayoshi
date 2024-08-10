c8c73ef133c8253a6e8204c23dc8e6af56ae982b
leon-do <leondo123@gmail.com>
2024-08-10 12:36:39 -0400

    silkscreen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,13 @@
 import type { Metadata } from "next";
-import { Rubik_Mono_One} from "next/font/google";
+import { Silkscreen } from "next/font/google";
 import "./globals.css";
 import { ThirdwebProvider } from "thirdweb/react";
 
-const font = Rubik_Mono_One({ weight: "400", subsets: ["latin"] });
+const font = Silkscreen({ weight: "400", subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Tamayoshi",
-  description:
-    "On Chain Click to Play Game",
+  description: "On Chain Click to Play Game",
 };
 
 export default function RootLayout({
